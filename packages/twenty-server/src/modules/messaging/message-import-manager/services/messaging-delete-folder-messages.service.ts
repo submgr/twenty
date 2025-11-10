4d950906eff0ad7d0d4cb7801a26c9bb6d9352e3
neo773 <neo773@protonmail.com>
2025-11-10 18:28:05 +0530

    wip

--- packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-delete-folder-messages.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-delete-folder-messages.service.ts
@@ -2,10 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 
 import chunk from 'lodash.chunk';
 import { isDefined } from 'twenty-shared/utils';
-import { In } from 'typeorm';
 
-import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
-import { MessageChannelMessageAssociationWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-channel-message-association.workspace-entity';
 import { type MessageChannelWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-channel.workspace-entity';
 import { type MessageFolderWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-folder.workspace-entity';
 import { MessagingMessageCleanerService } from 'src/modules/messaging/message-cleaner/services/messaging-message-cleaner.service';
@@ -18,7 +15,6 @@ export class MessagingDeleteFolderMessagesService {
   );
 
   constructor(
-    private readonly twentyORMGlobalManager: TwentyORMGlobalManager,
     private readonly messagingMessageCleanerService: MessagingMessageCleanerService,
     private readonly messagingGetMessageListService: MessagingGetMessageListService,
   ) {}
@@ -31,12 +27,6 @@ export class MessagingDeleteFolderMessagesService {
     this.logger.log(
       `WorkspaceId: ${workspaceId}, MessageChannelId: ${messageChannel.id}, FolderId: ${messageFolder.id} - Deleting messages from folder: ${messageFolder.name}`,
     );
-
-    const messageChannelMessageAssociationRepository =
-      await this.twentyORMGlobalManager.getRepositoryForWorkspace<MessageChannelMessageAssociationWorkspaceEntity>(
-        workspaceId,
-        'messageChannelMessageAssociation',
-      );
 
     const messageLists =
       await this.messagingGetMessageListService.getMessageLists(
@@ -56,36 +46,25 @@ export class MessagingDeleteFolderMessagesService {
       const messageExternalIdsChunks = chunk(messageExternalIds, 200);
 
       for (const messageExternalIdsChunk of messageExternalIdsChunks) {
-        const associations =
-          await messageChannelMessageAssociationRepository.find({
-            where: {
-              messageExternalId: In(messageExternalIdsChunk),
-              messageChannelId: messageChannel.id,
-            },
-            select: ['messageExternalId'],
-          });
+        const validExternalIds = messageExternalIdsChunk.filter(isDefined);
 
-        if (associations.length > 0) {
-          const externalIdsToDelete = associations
-            .map((assoc) => assoc.messageExternalId)
-            .filter(isDefined);
-
-          if (externalIdsToDelete.length > 0) {
-            await this.messagingMessageCleanerService.deleteMessagesChannelMessageAssociationsAndRelatedOrphans(
-              {
-                workspaceId,
-                messageExternalIds: externalIdsToDelete,
-                messageChannelId: messageChannel.id,
-              },
-            );
-
-            totalDeletedCount += externalIdsToDelete.length;
-
-            this.logger.log(
-              `WorkspaceId: ${workspaceId}, MessageChannelId: ${messageChannel.id}, FolderId: ${messageFolder.id} - Deleted ${externalIdsToDelete.length} messages from folder`,
-            );
-          }
+        if (validExternalIds.length === 0) {
+          continue;
         }
+
+        await this.messagingMessageCleanerService.deleteMessagesChannelMessageAssociationsAndRelatedOrphans(
+          {
+            workspaceId,
+            messageExternalIds: validExternalIds,
+            messageChannelId: messageChannel.id,
+          },
+        );
+
+        totalDeletedCount += validExternalIds.length;
+
+        this.logger.log(
+          `WorkspaceId: ${workspaceId}, MessageChannelId: ${messageChannel.id}, FolderId: ${messageFolder.id} - Processed ${validExternalIds.length} message deletions`,
+        );
       }
     }
 
